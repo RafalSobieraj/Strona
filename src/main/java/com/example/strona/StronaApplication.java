5b9e2d953d339185192d8dc556ee96b404ef899f
TheRafDay <rafal.sob@op.pl>
2021-09-02 13:18:45 +0200

    Initial commit

---
+++ src/main/java/com/example/strona/StronaApplication.java
@@ -0,0 +1,13 @@
+package com.example.strona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StronaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StronaApplication.class, args);
+    }
+
+}
