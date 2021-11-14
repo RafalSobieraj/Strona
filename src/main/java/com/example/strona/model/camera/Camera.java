651eaf22ddb7a485ff78bb4f3788795019a0624e
TheRafDay <rafal.sob@op.pl>
2021-11-14 22:31:58 +0100

    My Website

--- src/main/java/com/example/strona/model/camera/Camera.java
+++ src/main/java/com/example/strona/model/camera/Camera.java
@@ -90,7 +90,7 @@ public class Camera {
     public String getImagePath(){
         if(Image == null || id == null) return null;
 
-        return "/images/" + "cameras/" + id + "/" + Image;
+        return "/app/" + "images/" + "cameras/" + id + "/" + Image;
     }
 
     @Override
