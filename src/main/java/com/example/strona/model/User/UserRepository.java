de9e889810a9a617622d40f75b8323b144c01136
RafalSobieraj <rafal.sob@op.pl>
2022-05-13 15:34:30 +0200

    My Website

--- src/main/java/com/example/strona/model/User/UserRepository.java
+++ src/main/java/com/example/strona/model/User/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
 
-    @Query("SELECT u FROM User u WHERE u.username = :username")
+    @Query("SELECT user FROM User user WHERE user.username = :username")
     public User getUserByUsername(@Param("username") String username);
     
 }
