7f3f2d1c91039cf950457d2be95d0b029edc7e9f
RafalSobieraj <rafal.sob@op.pl>
2022-07-28 01:50:24 +0200

    My Website

--- src/main/java/com/example/strona/WebSecurityConfig.java
+++ src/main/java/com/example/strona/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,7 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
-    
+
     @Bean
     public UserDetailsService userDetailsService(){
         return new UserDetailsServiceImpl();
